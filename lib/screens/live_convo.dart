71243e3acdba0663e2fb3f2efc1feb49ce18f61e
Gaurav Katva <gauravkatva@gmail.com>
2021-03-03 14:42:40 +0530

    ui part has been done

--- lib/screens/live_convo.dart
+++ lib/screens/live_convo.dart
@@ -1,5 +1,7 @@
 import 'package:bouncing_widget/bouncing_widget.dart';
+import 'package:convoz/components/live_person.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LiveConvo extends StatefulWidget {
   @override
@@ -30,7 +32,107 @@ class _LiveConvoState extends State<LiveConvo> {
               child: SafeArea(
                 child: SingleChildScrollView(
                   physics: BouncingScrollPhysics(),
-                  child: Column(),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.menu,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 30,
+                              ),
+                              Text(
+                                "Live Convo",
+                                style: GoogleFonts.lato(
+                                  textStyle: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 20,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.pink[400],
+                              Colors.redAccent[200],
+                            ],
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "LIVE",
+                            style: GoogleFonts.lato(
+                              textStyle: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          LivePerson(
+                            imagePath: "assets/elon.png",
+                            name: "Elon Musk",
+                            isLive: true,
+                          ),
+                          LivePerson(
+                            imagePath: "assets/jeff.png",
+                            name: "Jeff Bezos",
+                            isLive: false,
+                          ),
+                          LivePerson(
+                            imagePath: "assets/bill.jpeg",
+                            name: "Bill Gates",
+                            isLive: false,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          LivePerson(
+                            imagePath: "assets/warren.png",
+                            name: "Warren Buffet",
+                            isLive: false,
+                          ),
+                          LivePerson(
+                            imagePath: "assets/oprah.png",
+                            name: "Oprah",
+                            isLive: false,
+                          ),
+                          LivePerson(
+                            imagePath: "assets/ryan.png",
+                            name: "Ryan Kumar",
+                            isLive: true,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
